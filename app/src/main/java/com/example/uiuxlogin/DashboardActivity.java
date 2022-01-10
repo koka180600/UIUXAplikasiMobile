96f6106c8fda3497926085372f009ae4189f2a7c
AsihIndriati <ratihindriati14@gmail.com>
2022-01-10 14:26:32 +0700

    LogoutActivity

--- app/src/main/java/com/example/uiuxlogin/DashboardActivity.java
+++ app/src/main/java/com/example/uiuxlogin/DashboardActivity.java
@@ -2,13 +2,25 @@ package com.example.uiuxlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    TextView logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        logout = (TextView) findViewById(R.id.LO);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, LogoutActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/uiuxlogin/MainActivity.java
+++ app/src/main/java/com/example/uiuxlogin/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+
             }
         });
 
